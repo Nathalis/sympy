407fa07ba84a4ddcaeee83f4bc5f238cbca7c31c
Matthew Rocklin <mrocklin@gmail.com>
2012-02-21 09:31:57 -0600

    added evaluate keyword handling to P()
    Added tests
    
    Fixed E(Add(...), evaluate=False) error

--- sympy/stats/tests/test_continuous_rv.py
+++ sympy/stats/tests/test_continuous_rv.py
@@ -1,7 +1,7 @@
 from sympy.stats import (Normal, LogNormal, Exponential, P, E, Where, Density,
         Var, Covar, Skewness, Gamma, Pareto, Weibull, Beta, Uniform, Given, pspace, CDF, ContinuousRV, Sample)
 from sympy import (Symbol, exp, S, N, pi, simplify, Interval, erf, Eq, symbols,
-        sqrt, And, gamma, beta, Piecewise)
+        sqrt, And, gamma, beta, Piecewise, Integral)
 from sympy.utilities.pytest import raises, XFAIL, slow
 
 oo = S.Infinity
@@ -283,3 +283,20 @@ def test_transform_density():
     z, dens = Density(X**2)
     assert Eq(dens, Piecewise((0, sqrt(z)<1), (0, sqrt(z)>10),
         (1/(18*sqrt(z)), True)))
+
+def test_unevaluated():
+    x = Symbol('x')
+    X = Normal(0,1, symbol=x)
+    assert E(X, evaluate=False) == \
+            Integral(sqrt(2)*x*exp(-x**2/2)/(2*sqrt(pi)), (x, -oo, oo))
+
+    assert E(X+1, evaluate=False) == \
+            Integral(sqrt(2)*x*exp(-x**2/2)/(2*sqrt(pi)), (x, -oo, oo)) + 1
+
+    assert P(X>0, evaluate=False).args == \
+            Integral(sqrt(2)*exp(-x**2/2)/(2*sqrt(pi)), (x, 0, oo)).args
+
+    assert P(X>0, X**2<1, evaluate=False).args == \
+            Integral(sqrt(2)*exp(-x**2/2)/(2*sqrt(pi)*
+            Integral(sqrt(2)*exp(-x**2/2)/(2*sqrt(pi)),
+                (x, -1, 1))), (x, 0, 1)).args
